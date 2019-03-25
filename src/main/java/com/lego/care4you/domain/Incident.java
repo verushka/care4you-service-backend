e2eae9478c2de94d3525351a13d3a69bf3277c9a
nicaela <verushka.or@gmail.com>
2019-03-25 02:41:21 -0400

    add-my-repository

---
+++ src/main/java/com/lego/care4you/domain/Incident.java
@@ -0,0 +1,27 @@
+package com.lego.care4you.domain;
+
+import com.lego.care4you.domain.bootstrap.GenericDomain;
+import com.lego.care4you.domain.enums.IncidentSeverity;
+import com.lego.care4you.domain.enums.IncidentType;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.Date;
+
+
+@Getter
+@Setter
+@Document
+public class Incident extends GenericDomain {
+
+    private String name;
+    private String description;
+    private Date date;
+    private IncidentType type;
+    private IncidentSeverity severity;
+
+    @DBRef
+    private Employee employee;
+}
---
+++ src/main/java/com/lego/care4you/dto/IncidentRequestDTO.java
@@ -0,0 +1,22 @@
+package com.lego.care4you.dto;
+
+import com.lego.care4you.domain.enums.IncidentSeverity;
+import com.lego.care4you.domain.enums.IncidentType;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+
+
+@Getter
+@Setter
+public class IncidentRequestDTO {
+
+    private String name;
+    private String description;
+    private Date date;
+    private IncidentType type;
+    private IncidentSeverity severity;
+
+    private String employeeId;
+}
